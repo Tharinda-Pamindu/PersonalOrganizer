e781b6da9431fb2055855b9fae055369f74763c1
Tharinda <tharipami2k20@gmail.com>
2025-01-12 01:20:29 +0530

    full project

---
+++ PersonalOrganizer/Info.h
@@ -0,0 +1,111 @@
+#pragma once
+
+namespace PersonalOrganizer {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Info
+	/// </summary>
+	public ref class Info : public System::Windows::Forms::Form
+	{
+	public:
+		Info(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Info()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	protected:
+
+
+
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Info::typeid));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(492, 526);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(101, 45);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"OK";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Info::button1_Click);
+			// 
+			// label1
+			// 
+			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::Fixed3D;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Sitka Banner", 10.2F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(586, 505);
+			this->label1->TabIndex = 5;
+			this->label1->Text = resources->GetString(L"label1.Text");
+			// 
+			// Info
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 33);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->ClientSize = System::Drawing::Size(610, 581);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->button1);
+			this->DoubleBuffered = true;
+			this->Font = (gcnew System::Drawing::Font(L"Sitka Display", 13.8F, System::Drawing::FontStyle::Bold));
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::Fixed3D;
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
+			this->Margin = System::Windows::Forms::Padding(4, 6, 4, 6);
+			this->MaximizeBox = false;
+			this->MinimizeBox = false;
+			this->Name = L"Info";
+			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
+			this->Text = L"Info";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		this->Close();
+	}
+	};
+}
